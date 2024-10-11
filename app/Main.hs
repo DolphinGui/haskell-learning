b35782a731fc01f8f54deeda08f6b6b4fd3085ac
Shin Umeda <umeda.shin@gmail.com>
2024-10-10 17:14:53 -0700

    reducer reduces expressions to a single number now

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TupleSections #-}
 module Main where
 
 import Data.Char
@@ -61,6 +62,12 @@ parseNum tok
     (_, Nothing) -> Nothing
     (Just x, Just y) -> Just (x : y)
 
+(?++) :: Maybe [a] -> Maybe [a] -> Maybe [a]
+(?++) a b = case (a, b) of
+    (Nothing, _) -> Nothing
+    (_, Nothing) -> Nothing
+    (Just x, Just y) -> Just (x ++ y)
+
 (||?) :: (a -> Maybe b) -> (a -> Maybe b) -> (a -> Maybe b)
 (||?) b c a =  case b a of
     Just x -> Just x
@@ -105,19 +112,21 @@ isSubexpr e = case e of
     Subexpression _ -> True
     _ -> False
 
-eval :: Context -> Expression -> (Maybe Double, Context)
-eval ct expr =  case expr of
-    Number n -> (Just n, ct)
-    Variable v -> (lookup v ct, ct)
-    Subexpression sub -> reduce ct sub
-    _ -> (Nothing, ct)
+eval :: (Expression, Context) -> Maybe (Double, Context)
+eval (expr, ct) =  case expr of
+    Number n -> Just (n, ct)
+    Variable v -> case lookup v ct of
+        Just n -> Just (n, ct)
+        Nothing -> Nothing
+    Subexpression sub -> reduce (sub, ct)
+    _ -> Nothing
 
-getOp :: Operator -> Context -> Expression -> Expression -> (Maybe Expression, Context)
-getOp op ct = \lhs rhs -> case eval ct lhs of
-    (Just left, ct2) -> case eval ct2 rhs of
-        (Just right, ct3) -> (Just . Number $ getOp' op left right, ct3)
-        (Nothing, _) -> (Nothing, ct)
-    (Nothing, _) -> (Nothing, ct)
+getOp :: Operator -> Context -> Expression -> Expression -> Maybe (Expression, Context)
+getOp op ct = \lhs rhs -> case eval (lhs, ct) of
+    Just (l, ct1) -> case eval (rhs, ct1) of
+        Just (r, ct2) -> Just (Number $ getOp' op l r, ct2)
+        Nothing -> Nothing
+    Nothing -> Nothing
     where
         getOp' :: Operator -> Double -> Double -> Double
         getOp' Add = (+)
@@ -127,19 +136,36 @@ getOp op ct = \lhs rhs -> case eval ct lhs of
         getOp' Exp = (**)
         getOp' Assign = error "Assign should never be called on this"
 
-reduce :: Context -> [Expression] -> (Maybe Double, Context)
-reduce ct [] = (Nothing, ct)
-reduce ct [e] = (case e of
-    Number n -> Just n
-    _ -> Nothing
-    , ct)
-reduce ct [_, _] = (Nothing, ct)
-reduce ct (lhs : (op : (rhs : es))) = case op of
-    Operator o -> case getOp o ct lhs rhs of
-        (Just e, c) -> reduce c (e : es)
-        (Nothing, c)  -> (Nothing, c)
-    _ -> (Nothing, ct)
+window :: ((a, a, a) -> Maybe [a]) -> [a] -> Maybe [a]
+window f arr
+    | (lhs : (mid : ( rhs : rest))) <- arr = case f (lhs, mid, rhs) of
+        Just r ->  Just r ?++ window f rest
+        Nothing -> Nothing
+    | _ <- arr = Just []
 
+concatCt :: Expression -> Maybe ([Expression], Context) -> Maybe ([Expression], Context)
+concatCt _ Nothing = Nothing
+concatCt left (Just (right, ct)) = Just (left : right, ct)
+
+reduce' :: Operator ->  ([Expression], Context) -> Maybe ([Expression], Context)
+reduce' _ ([], ct) = Just ([], ct)
+reduce' _ ([a], ct) = Just ([a], ct)
+reduce' _ ([a, b], ct) = Just ([a,b], ct)
+reduce' o (lhs: (mid : (rhs : es)), ct)
+    | Operator op <- mid = if op == o
+        then case getOp o ct lhs rhs of
+            Nothing -> Nothing
+            Just (e, ctnew) -> reduce' o (e : es, ctnew)
+        else concatCt lhs $ reduce' o (mid : rhs : es, ct)
+    | otherwise = concatCt lhs $ reduce' o (mid : rhs : es, ct)
+
+reduce :: ([Expression], Context) -> Maybe (Double, Context)
+reduce iii = case reduce' Exp iii >>= reduce' Mult >>= reduce' Div >>= reduce' Add >>= reduce' Sub
+    of
+    Just (expFinal, ctFinal) -> case expFinal of
+        [fin] -> eval (fin, ctFinal)
+        _ -> Nothing
+    Nothing -> Nothing
 
 printmany :: [String] -> IO [()]
 printmany = mapM putStrLn
@@ -151,10 +177,11 @@ printmany = mapM putStrLn
 main :: IO ()
 main = do
     putStrLn "Hello, Haskell!"
-    let a = parse $ tokenize "2 + (3 - 12) ^ 2"
-    let ct = []
-    print a
-    print (a>>= fst . reduce ct)
+    let a = parse $ tokenize "(2 - 3) / 2"
+    case a of
+        Just e -> print $ reduce (e, [])
+        Nothing -> print "Nothing"
+
     return ()
 
 
